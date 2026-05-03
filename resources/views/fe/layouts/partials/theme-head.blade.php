139b10f719e92d391227f3f3e9d81dc71e804a7f
Rajindra Sirhan <145415302+kamarov28@users.noreply.github.com>
2026-05-03 20:53:00 +0700

    Use SprintLog artwork as browser tab icon

--- resources/views/fe/layouts/partials/theme-head.blade.php
+++ resources/views/fe/layouts/partials/theme-head.blade.php
@@ -1,3 +1,6 @@
+<link rel="icon" type="image/png" href="/artwork/logo.png">
+<link rel="shortcut icon" type="image/png" href="/artwork/logo.png">
+<link rel="apple-touch-icon" href="/artwork/logo.png">
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Montserrat:wght@500;600;700;800&family=Nunito:wght@500;600;700;800&family=Space+Grotesk:wght@500;600;700&display=swap" rel="stylesheet">
